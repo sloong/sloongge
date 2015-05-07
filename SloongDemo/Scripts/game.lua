d0760e6d393c2a914b8d657944b59eb6151d593e
Wang Chuanbin <Wang.Chuanbin@sloong.com>
2015-05-07 18:03:27 +0800

    添加Keyboard定义

--- SloongDemo/Scripts/game.lua
+++ SloongDemo/Scripts/game.lua
@@ -23,10 +23,18 @@ for k,v in pairs(g_Game_GUI_Item) do
 	Load3DModule( k, v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10]  ) --參數依次爲物體保存的ID,模型文件,世界座標,縮放大小,旋轉係數
 end
 
---RegisterEvent("GameEventHandler")
---function GameEventHandler( id, event )
---	if id ~= 0 then
---		local fun = g_Game_GUI_Item[id]['Hanlder'];
---		if nil ~= fun then fun(event) end;
---	end
---end
+RegisterKeyboard = 
+{
+	Keyboard['DIK_LEFT'],
+	Keyboard['DIK_RIGHT'],
+	Keyboard['DIK_UP'],
+	Keyboard['DIK_DOWN'],
+}
+RegisterKeyboardEvent(RegisterKeyboard);
+RegisterEvent("GameEventHandler")
+function GameEventHandler( id, event )
+	if id ~= 0 then
+		local fun = g_Game_GUI_Item[id]['Hanlder'];
+		if nil ~= fun then fun(event) end;
+	end
+end
